4a8e8cca418c177c7d54f29f5577a391642a2ff7
sungsik86 <94594017+sungsik86@users.noreply.github.com>
2023-08-02 10:28:23 +0900

    Create recombination.java
    //입력된 String 과 index로 문자 재 조합

---
+++ recombination.java
@@ -0,0 +1,18 @@
+import java.util.*;
+//입력된 String 과 index로 문자 재 조합
+class Solution {
+    public String solution(String my_string, int[] index_list) {
+        String answer = "";
+        
+        char [] myString = new char[my_string.length()];
+        for(int i =0 ; i < myString.length ; i++){
+            System.out.print(my_string.charAt(i));
+            myString[i] = my_string.charAt(i);
+        }
+        for(int a : index_list){
+            char k = myString[a];
+            answer += k;
+        }
+        return answer;
+    }
+}
