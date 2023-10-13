b3b2fc5a5d5a747516631aa6fb4b151475d4b584
sungsik86 <94594017+sungsik86@users.noreply.github.com>
2023-10-13 12:58:01 +0900

    Create DataBaseConfig.java

---
+++ java/toJpaCode/DataBaseConfig.java
@@ -0,0 +1,73 @@
+package com.shinhands.smms.database;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+//configuration annotation
+
+
+@EnableJpaRepositories(
+	    basePackages="com.happy.people.core.repo",  //repository를 관리할 패키지 명시
+	    entityManagerFactoryRef = "entityManagerFactory", //EntityManagerFactory
+	    transactionManagerRef = "transactionManager") // transactionManager
+public class DataBaseConfig {
+	private static final String DEFAULT_NAMING_STRATEGY
+		= "org.springframework.boot.orm.jpa.hibernate.SpringPhysicalNamingStrategy";
+	
+	@Bean
+	@Primary //해당 Bean을 우선적으로 선택하도록 하는 annotation
+	public DataSource defaultDataSource() {
+		HikariConfig dataSourceConfig = new HikariConfig();
+		dataSourceConfig.setDriverClassName("com.mysql.cj.jdbc.Driver");
+	    dataSourceConfig.setJdbcUrl("jdbc:mysql://127.0.0.1:3306/{databaseName}?characterEncoding=UTF-8&serverTimezone=UTC&allowMultiQueries=true");
+	    dataSourceConfig.setUsername("id");
+	    dataSourceConfig.setPassword("password");
+	    dataSourceConfig.setMaximumPoolSize(10);
+	    dataSourceConfig.setMinimumIdle(5);
+	    dataSourceConfig.setMaxLifetime(1200000);
+	    dataSourceConfig.setConnectionTimeout(20000	);
+	    dataSourceConfig.setIdleTimeout(300000);
+	    
+	    return new HikariDataSource(dataSourceConfig);
+	}
+	
+	@Bean(name = "entityManagerFactory")
+	@Primary
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory(
+	    EntityManagerFactoryBuilder builder) {
+	
+	  Map<String, String> propertiesHashMap = new HashMap<>();
+	  propertiesHashMap.put("hibernate.physical_naming_strategy",DEFAULT_NAMING_STRATEGY);
+
+	  LocalContainerEntityManagerFactoryBean rep =
+	  builder.dataSource(defaultDataSource())
+	    .packages("com.happy.people.core.domain")
+        //domain을 관리할 패키지 경로 명시 (domain = DO 파일)
+	    .properties(propertiesHashMap)
+	    .build();
+	 
+	  return rep;
+	}
+
+	@Primary
+	@Bean(name = "transactionManager")
+	PlatformTransactionManager transactionManager(
+	    EntityManagerFactoryBuilder builder) {
+	  return new JpaTransactionManager(entityManagerFactory(builder).getObject());
+	}
+
+}
